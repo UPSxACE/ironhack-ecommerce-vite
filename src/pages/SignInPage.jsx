555dcf61a96ca8b4065bca64e64996eb61b6e545
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-17 13:59:52 +0200

    SignIn Sign Up okayish with Input component with props

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import creativeReels from '../assets/creative-reels-composition.jpg'
+import creativeReels from "../assets/creative-reels-composition.jpg";
+import Input from "../components/Input";
 
 function SignInPage() {
   return (
-    <div>
-      <img src={creativeReels}></img>
-      <form>
-        <label>Sign In</label>
-        <input type="email" placeholder="email"></input>
-        <input type="password" placeholder="password"></input>
+    <div className="flex flex-row space-x-80 p-10">
+      <img src={creativeReels} className="w-3/6"></img>
+      <form className="w-3/6">
+        <div className="flex flex-col my-40">
+          <label className="text-4xl py-6 font-semibold">Sign In</label>
+          <Input type="email" placeholder="Email"></Input>
+          <Input type="password" placeholder="Password"></Input>
+          <div className="flex items-center justify-around p-8">
+            <button className="btn bg-black text-white py-4 px-12 text-3xl">Log In</button>
+            <p className="p-3">Forgot Password?</p>
+          </div>
+        </div>
       </form>
     </div>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import creativeReels from "../assets/creative-reels-composition.jpg";
+import Input from "../components/Input";
 
 function SignUpPage() {
   return (
-    <div>SignUpPage</div>
-  )
+    <div className="flex flex-row space-x-80 p-10">
+      <img src={creativeReels} className="w-3/6"></img>
+      <form className="w-3/6">
+        <div className="flex flex-col my-40">
+          <label className="text-4xl py-6 font-semibold">Sign Up</label>
+          <Input type="text" placeholder="Name"></Input>
+          <Input type="email" placeholder="Email"></Input>
+          <Input type="password" placeholder="Password"></Input>
+          <Input type="password" placeholder="Repeat Password"></Input>
+          <button className="btn bg-black text-white py-4 px-10 text-3xl w-3/5 my-6">Register</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SignUpPage
+export default SignUpPage;
