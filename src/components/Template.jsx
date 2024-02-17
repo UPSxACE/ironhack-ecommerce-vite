a3a8aa189625ad57b81e5fd598e5996e99fa86f1
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-17 17:28:21 +0200

    Sign In Sign Up and Template for Navbar ready

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,27 +1,33 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { IoHeartOutline } from "react-icons/io5";
+import { PiShoppingCartThin } from "react-icons/pi";
 
 export default function Template() {
   let location = useLocation();
 
   return (
     <div>
-      <header>
-        <h2>IronTech</h2>
-        <nav>
-          <Link to={'/'}>
+      <header className="flex justify-between border-b h-[50px] items-center">
+        <h2 className="font-semibold ml-4">IronTech</h2>
+        <nav className="flex gap-12">
+          <NavLink to={"/"}>
             <p>Home</p>
-          </Link>
-          <Link to={'/products'}>
+          </NavLink>
+          <NavLink to={"/products"}>
             <p>Products</p>
-          </Link>
-          <Link to={'/sign-in'}>
+          </NavLink>
+          <NavLink to={"/sign-in"}>
             <p>Sign In</p>
-          </Link>
-          <Link to={'/sign-up'}>
+          </NavLink>
+          <NavLink to={"/sign-up"}>
             <p>Sign Up</p>
-          </Link>
+          </NavLink>
         </nav>
+        <div className="flex mr-5 gap-5 items-centerx">
+          <IoHeartOutline></IoHeartOutline>
+          <PiShoppingCartThin></PiShoppingCartThin>
+        </div>
       </header>
 
       <Outlet />
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,10 +4,10 @@ import Input from "../components/Input";
 
 function SignUpPage() {
   return (
-    <div className="flex flex-row space-x-80 p-10">
-      <img src={creativeReels} className="w-3/6"></img>
-      <form className="w-3/6">
-        <div className="flex flex-col my-40">
+    <div className="flex flex-row p-10 min-h-[calc(100vh-50px)] ">
+      <img src={creativeReels} className="w-3/6 object-cover"></img>
+      <form className="w-3/6 flex flex-col items-center">
+        <div className="flex flex-col justify-center h-full max-w-[500px] min-w-[450px]">
           <label className="text-4xl py-6 font-semibold">Sign Up</label>
           <Input type="text" placeholder="Name"></Input>
           <Input type="email" placeholder="Email"></Input>
