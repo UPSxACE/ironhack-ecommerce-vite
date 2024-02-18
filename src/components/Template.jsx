9463cc0bf127197e0e753df31519a91ada5b42c9
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-18 22:46:24 +0200

    responsive Template navabar done

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,35 +2,71 @@ import { Link, Outlet, useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { IoHeartOutline } from "react-icons/io5";
 import { PiShoppingCartThin } from "react-icons/pi";
-import { CiMenuBurger } from "react-icons/ci"
+import { CiMenuBurger } from "react-icons/ci";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function Template() {
   let location = useLocation();
+  function activeClass({ isActive, isPending }) {
+    return   isPending ? "pending" : isActive ? "underline underline-offset-2": ""
+  }
 
   return (
     <div>
       <header className="flex justify-center border-b">
         <div className="flex justify-between h-[49px] items-center max-w-screen-lg w-full">
           <Link className="font-semibold">IronTech</Link>
-            <CiMenuBurger className="flex sm:hidden">
-            </CiMenuBurger>
           <nav className="hidden gap-12 sm:flex">
-            <NavLink to={"/"}>
-              <p>Home</p>
+            <NavLink to={"/"} className={activeClass}>
+              <span>Home</span>
             </NavLink>
-            <NavLink to={"/products"}>
-              <p>Products</p>
+
+            <NavLink to={"/products"} className={activeClass}>
+              <span>Products</span>
             </NavLink>
-            <NavLink to={"/sign-in"}>
-              <p>Sign In</p>
+
+            <NavLink to={"/sign-in"} className={activeClass}>
+              <span>Sign In</span>
             </NavLink>
-            <NavLink to={"/sign-up"}>
-              <p>Sign Up</p>
+
+            <NavLink to={"/sign-up"} className={activeClass}>
+              <span>Sign Up</span>
             </NavLink>
           </nav>
-          <div className="flex gap-5 items-center text-xl">
-            <IoHeartOutline></IoHeartOutline>
-            <PiShoppingCartThin></PiShoppingCartThin>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex sm:hidden p-2">
+              <CiMenuBurger></CiMenuBurger>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="sm:hidden">
+              <DropdownMenuItem>
+                <NavLink to={"/"}>
+                  <span>Home</span>
+                </NavLink>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <NavLink to={"/products"}>
+                  <span>Products</span>
+                </NavLink>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <NavLink to={"/sign-in"}>
+                  <span>Sign In</span>
+                </NavLink>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <NavLink to={"/sign-up"}>
+                  <span>Sign Up</span>
+                </NavLink>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div className="flex gap-1 items-center text-xl">
+            <Link to="/" className="p-2">
+              <IoHeartOutline></IoHeartOutline>
+            </Link>
+            <Link to="/" className="p-2">
+              <PiShoppingCartThin></PiShoppingCartThin>
+            </Link>
           </div>
         </div>
       </header>
