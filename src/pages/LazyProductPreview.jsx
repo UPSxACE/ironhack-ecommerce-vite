5989932fb84f9dc41466eaf595075039137f47da
Eduardo Botelho <eduardo11224b@gmail.com>
2024-03-07 20:47:32 +0000

    skeleton on favourites product page

--- src/pages/LazyProductPreview.jsx
+++ src/pages/LazyProductPreview.jsx
@@ -1,17 +1,42 @@
 import ProductPreview from "@/components/ProductPreview";
+import ProductPreviewSkeleton from "@/components/ProductPreviewSkeleton";
 import useGetRequest from "@/hooks/use-get-request";
-import React, { useEffect, useState } from "react";
 
 function LazyProductPreview({ productId }) {
-  
-  const { result: product, done, error } = useGetRequest(`/products/${productId}`, { _embed: "rates"});
+  const {
+    result: product,
+    done,
+    error,
+  } = useGetRequest(`/products/${productId}`, { _embed: "rates" });
 
   const rateCount = product?.rates ? product.rates.length : 0;
 
-  const rateSum = rateCount > 0 ? product.rates.reduce((accRate, currRate) => accRate + currRate.rating, 0) : 0;
+  const rateSum =
+    rateCount > 0
+      ? product.rates.reduce(
+          (accRate, currRate) => accRate + currRate.rating,
+          0
+        )
+      : 0;
   const rateAverage = Math.floor(rateSum / rateCount);
 
-  return <ProductPreview id={product?.id} imageUrl={product?.image} title={product?.title} price={product?.price} rating={rateAverage} ratingCount={rateCount} linkClass={"w-full"} articleClass={"w-full"} imageWrapperClass={"w-full"} />;
+  if (!done || error) {
+    return <ProductPreviewSkeleton />;
+  }
+
+  return (
+    <ProductPreview
+      id={product?.id}
+      imageUrl={product?.image}
+      title={product?.title}
+      price={product?.price}
+      rating={rateAverage}
+      ratingCount={rateCount}
+      linkClass={"w-full"}
+      articleClass={"w-full"}
+      imageWrapperClass={"w-full"}
+    />
+  );
 }
 
 export default LazyProductPreview;
